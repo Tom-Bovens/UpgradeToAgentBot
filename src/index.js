f9bc1f5bd672029e17eb4233374243c3cae04439
Tom Bovens <Tombovens2003@gmail.com>
2020-09-27 16:01:30 +0200

    More work done.

--- src/index.js
+++ src/index.js
@@ -40,180 +40,135 @@ bot.on('error', log);
 
 bot.on('assign', async (message, conversation) => {
     if (conversation.type === 'agent') {
-        try {
-            const user = conversation.participants.find((p) => p.role === 'agent');
-            if (user) {
-                bot.send(conversation.id, [
-                    {
-                        text: 'Hello! Do you want to be upgraded to the Agent role?',
-                        role: 'bot',
-                        delay: incrementor.set(3),
-                        actions: [
-                            {
-                                type: 'reply',
-                                text: 'Yes!',
-                                payload: 'AskForUpgrade'
-                            },
-                            {
-                                type: 'reply',
-                                text: 'No.',
-                                payload: 'Cancel'
-                            }
-                        ]
-                    }
-                ]);
-            }
-        } catch (e) {
-            errorCatch(e);
+        const user = conversation.participants.find((p) => p.role === 'agent').catch((err) => { errorCatch(err, console.trace()); });
+        if (user) {
+            bot.send(conversation.id, [
+                {
+                    text: 'Hello! Do you want to be upgraded to the Agent role?',
+                    role: 'bot',
+                    delay: incrementor.set(3),
+                    actions: [
+                        {
+                            type: 'reply',
+                            text: 'Yes!',
+                            payload: 'AskForUpgrade'
+                        },
+                        {
+                            type: 'reply',
+                            text: 'No.',
+                            payload: 'Cancel'
+                        }
+                    ]
+                }
+            ]).catch((err) => { errorCatch(err, console.trace()); });
         }
     }
 });
 
 bot.on('message.create.*.postback', async (message, conversation) => {
-    try {
-        if (message.text === 'AskForUpgrade') {
-            const user = conversation.participants.find((p) => p.role === 'agent');
-            const adminColleagues = await bot.users.list({ organization: user.organization, role: 'admin' || 'owner', limit: 10 });
-            const adminArray = [];
-            for await (const adminUser of adminColleagues) {
-                try {
-                    if (adminUser.status === 'active') {
-                        const arrayElement = {
-                            type: 'reply',
-                            text: adminUser.displayName,
-                            payload: `adminToAsk:${adminUser.id}`
-                        };
-                        adminArray.push(arrayElement);
-                    }
-                } catch (e) {
-                    errorCatch(e);
+    if (message.text === 'AskForUpgrade') {
+        const user = conversation.participants.find((p) => p.role === 'agent').catch((err) => { errorCatch(err, console.trace()); });
+        const adminColleagues = await bot.users.list({ organization: user.organization, role: 'admin' || 'owner', limit: 10 }).catch((err) => { errorCatch(err, console.trace()); });
+        const adminArray = [];
+        for await (const adminUser of adminColleagues) {
+            try {
+                if (adminUser.status === 'active') {
+                    const arrayElement = {
+                        type: 'reply',
+                        text: adminUser.displayName,
+                        payload: `adminToAsk:${adminUser.id}`
+                    };
+                    adminArray.push(arrayElement);
                 }
+            } catch (e) {
+                errorCatch(e);
             }
-            await conversation.say({
-                text: "Alright! I'll have to ask an admin for permission before I can upgrade you. Which admin do you want me to ask?",
-                role: 'bot',
-                delay: incrementor.set(3),
-                actions: adminArray
-            });
-        } else if (message.text.match('adminToAsk')) {
-            const user = conversation.participants.find((p) => p.role === 'agent');
-            const id = message.text.slice(11);
-            const adminUser = await bot.users.get(id);
-            await conversation.say({
-                text: `Alright! I'll ask ${adminUser.givenName} for permission. In the mean time, you'll just have to wait.`,
-                role: 'bot',
-                delay: incrementor.set(3)
-            });
-            const adminconversation = await bot.conversations.create(
-                { name: 'Upgrade guest to Agent', messages: [{ text: `Hey ${adminUser.givenName}!` }] }
-            );
-            conversation.set('adminConvUrl', adminconversation.id);
-            await bot.send(adminconversation.id, [{
-                text: `I've come to forward a request from the Guest account ${user.name} to have their account upgraded to Agent stats.`,
-                delay: incrementor.set(4),
-                actions: [
-                    {
-                        type: 'reply',
-                        text: 'Accept upgrade.',
-                        payload: 'AcceptUpgrade'
-                    },
-                    {
-                        type: 'reply',
-                        text: 'Deny upgrade.',
-                        payload: 'CancelUpgrade'
-                    }
-                ]
-            },
-            {
-                type: 'command',
-                text: '/assign',
-                meta: {
-                    users: [
-                        adminUser.id
-                    ]
-                }
-            }]);
-        } else if (message.text.match('accepted-')) {
-            await bot.send(adminconversation.id, [{
-                text: `I've come to forward a request from the Guest account ${user.name} to have their account upgraded to Agent stats.`,
-                delay: incrementor.set(4),
-                actions: [
-                    {
-                        type: 'reply',
-                        text: 'Accept upgrade.',
-                        payload: 'AcceptUpgrade'
-                    },
-                    {
-                        type: 'reply',
-                        text: 'Deny upgrade.',
-                        payload: 'CancelUpgrade'
-                    }
-                ]
-            },
-            {
-                type: 'command',
-                text: '/assign',
-                meta: {
-                    users: [
-                        adminUser.id
-                    ]
-                }
-            }]);
         }
-    } catch (e) {
-        errorCatch(e);
+        await conversation.say({
+            text: "Alright! I'll have to ask an admin for permission before I can upgrade you. Which admin do you want me to ask?",
+            role: 'bot',
+            delay: incrementor.set(3),
+            actions: adminArray
+        }).catch((err) => { errorCatch(err, console.trace()); });
+    } else if (message.text.match('adminToAsk')) {
+        const user = conversation.participants.find((p) => p.role === 'agent').catch((err) => { errorCatch(err, console.trace()); });
+        const id = message.text.slice(11);
+        const adminUser = await bot.users.get(id).catch((e) => { errorCatch(e, console.trace()); });
+        await conversation.say({
+            text: `Alright! I'll ask ${adminUser.givenName} for permission. In the mean time, you'll just have to wait.`,
+            role: 'bot',
+            delay: incrementor.set(3)
+        }).catch((err) => { errorCatch(err, console.trace()); });
+        const adminconversation = await bot.conversations.create(
+            { name: 'Upgrade guest to Agent', messages: [{ text: `Hey ${adminUser.givenName}!` }] }
+        ).catch((err) => { errorCatch(err, console.trace()); });
+        conversation.set('adminConvUrl', adminconversation.id).catch((err) => { errorCatch(err, console.trace()); });
+        await bot.send(adminconversation.id, [{
+            text: `I've come to forward a request from the Guest account ${user.name} to have their account upgraded to Agent stats.`,
+            delay: incrementor.set(4),
+            actions: [
+                {
+                    type: 'reply',
+                    text: 'Accept upgrade.',
+                    payload: `accepted-${conversation.id}`
+                },
+                {
+                    type: 'reply',
+                    text: 'Deny upgrade.',
+                    payload: `denied-${conversation.id}`
+                }
+            ]
+        },
+        {
+            type: 'command',
+            text: '/assign',
+            meta: {
+                users: [
+                    adminUser.id
+                ]
+            }
+        }]).catch((err) => { errorCatch(err, console.trace()); });
+    } else if (message.text.match('accepted-')) {
+        await bot.send(conversation.id, [{
+            text: "Great! I'll inform him of your decision and will upgrade them to an agent account.",
+            delay: incrementor.set(4)
+        }]).catch((err) => { errorCatch(err, console.trace()); });
+        const guestConvId = parseInt(message.text.slice(9), 10);
+        await bot.send(guestConvId, [{
+            text: 'Hi there! Person has approved your upgrade to an Agent account! You now have access to all the magical features that ChatShipper has to offer!',
+            delay: incrementor.set(3)
+        }]).catch((err) => { errorCatch(err, console.trace()); });
+    } else if (message.text.match('denied-')) {
+        await bot.send(conversation.id, [{
+            text: 'Too bad then. What reason should I convey to them?',
+            delay: incrementor.set(4)
+        }]).catch((err) => { errorCatch(err, console.trace()); });
+        const guestConvId = parseInt(message.text.slice(9), 10);
+        conversation.set('guestConvId', guestConvId).catch((err) => { errorCatch(err, console.trace()); });
     }
 });
 
 bot.on('message.create.*.chat.*', async (message, conversation) => {
-    try {
-        const status = conversation.get('bugreportstatus');
-        if (status === 'Bug') {
-            await conversation.set('bugreportstatus', 'Replication');
-            await conversation.set('bugString', message.text);
-            await conversation.say({
-                contentType: 'text/html',
-                text: 'Can you give a quick description on how to reconstruct the issue? This is a pretty important step so describe it in detail.'
-            });
-        } else if (status === 'Replication' === true) {
-            await conversation.set('replicationString', message.text);
-            await conversation.set('bugreportstatus', 'Image');
-            await conversation.say({
-                contentType: 'text/html',
-                text: "And do you happen to have an image of the bug? If you have one, upload it in the main channel. If you don't have one, just say anything else and I'll skip ahead."
-            });
-        } else if (status === 'Image') {
-            let imageURL = message.text;
-            const match = imageURL.match('http');
-            if (match === null) {
-                imageURL = 'N/A';
-            }
-            await conversation.set('imageString', imageURL);
-            await conversation.set('bugreportstatus', 'Off');
-            const buggyString = conversation.get('bugString');
-            const replicativeString = conversation.get('replicationString');
-            const imageUrlString = conversation.get('imageString');
-            await conversation.say(
-                {
-                    contentType: 'text/html',
-                    text: `Is this the bug report you want to submit? <i></br></br><b>Issue description</b>:</br>${buggyString}</br></br><b>Steps to replicate:</b></br>${replicativeString}</br></br><b>Image of the issue</b>:</br>${imageUrlString}</i>`,
-                    actions: [
-                        {
-                            type: 'reply',
-                            text: 'Yes, send it off.',
-                            payload: 'ContinueReport'
-                        },
-                        {
-                            type: 'reply',
-                            text: 'No, cancel the report.',
-                            payload: 'CancelReport'
-                        }
-                    ]
-                }
-            );
+    const ID = await conversation.get('guestConvId').catch((err) => { errorCatch(err, console.trace()); });
+    if (ID) {
+        const reasonToGive = message.text;
+        await bot.send(ID, [{
+            text: 'Hey. Person has declined your upgrade to the Agent role.',
+            delay: incrementor.set(3)
+        },
+        {
+            text: "Here's the reason he gave for declining:",
+            delay: incrementor.delay(3)
+        },
+        {
+            text: reasonToGive,
+            delay: incrementor.delay(5)
+        },
+        {
+            text: 'Take it up with Person if you have any gripes, maybe you can convince him. Have a nice day!',
+            delay: incrementor.delay(3)
         }
-    } catch (e) {
-        errorCatch(e);
+        ]).catch((err) => { errorCatch(err, console.trace()); });
     }
 });
 
